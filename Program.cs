abc65f7dd96ab21d20c7d418dd4655fcbe0b9b1f
AbrahamRJ04 <abrahammjimenez@hotmail.com>
2024-07-08 23:59:50 -0600

    REJO: Configuracion Program y Creacion de Controlador

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using CrudReact.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DBCallCenterContext>();
 
 var app = builder.Build();
 
